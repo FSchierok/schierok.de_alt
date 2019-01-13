fff96299ad6cc91d75133c49e08bc4c779b0f71f
Fabian Schierok <fabian.schierok@tu-dortmund.de>
2019-01-13 11:45:50 +0100

    add point table

--- myside/tippspiel/views.py
+++ myside/tippspiel/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from . import sniffer
-from tippspiel.models import Event, Match, Tip, Profil
+from tippspiel.models import Event, Match, Tip, Point
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,6 +13,9 @@ def index(request):
             if not Event.objects.filter(title=newEvent[0]).exists():
                 E = Event(title=newEvent[0], id=newEvent[1])
                 E.save()
+                for user in User.objects.all():
+                    P = Point(user=user, event=E, points=0)
+                    P.save()
     return render(request, "tippspiel/index.html", {"running": Event.objects.all()})
 
 
@@ -21,7 +25,6 @@ def event(request, id):  # Event anzeigen
         for key in req.keys():
             if key != "csrfmiddlewaretoken":
                 tip = key.split(".")
-                print(tip)
                 T = Tip(user=request.user, tip=bool(
                     tip[1]), match=Match.objects.get(id=tip[0]))
                 T.save()
@@ -42,10 +45,11 @@ def event(request, id):  # Event anzeigen
                     match.result1 = bool(result[1])
                     match.result2 = not bool(result[1])
                     match.save()
-                    for profil in Profil.objects.all():
-                        if Tip.objects.get(user=profil.user, match=match).tip == bool(match.result2):
-                            profil.points += 1
+                    for user in User.objects.all():
+                        if Tip.objects.get(user=user, match=match).tip == bool(match.result2):
+                            Point.objects.get(
+                                user=user, event=match.event).points += 1
 
-        return render(request, "tippspiel/event.html", {"title": Event.objects.get(id=id).title, "matches": Match.objects.filter(event__id__contains=id, finished=False), "username": request.user.get_username, "points": Profil.objects.get(user=request.user).points, "result": Match.objects.filter(event__id__contains=id, finished=True)})
+        return render(request, "tippspiel/event.html", {"title": Event.objects.get(id=id).title, "matches": Match.objects.filter(event__id__contains=id, finished=False), "username": request.user.get_username, "points": Point.objects.get(user=request.user, event=Event.objects.get(id=id)).points, "result": Match.objects.filter(event__id__contains=id, finished=True)})
     else:
         return redirect("/u/login")
