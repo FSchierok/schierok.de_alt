b52cba2d112f49325e2e71cf61672c050e0520c8
Fabian Schierok <fabian.schierok@udo.edu>
2019-02-12 13:06:05 +0100

    fix re path again

--- myside/myside/urls.py
+++ myside/myside/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +26,8 @@ urlpatterns = [
     path("tippspiel/", include("tippspiel.urls")),
     path("", views.index),
     path(r'u/', include('django.contrib.auth.urls')),
-    path(r"media/",'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
  
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
