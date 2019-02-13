60e8dbd380bee5e74a013fd267a63fa2a5522055
Fabian Schierok <fabian.schierok@tu-dortmund.de>
2019-02-13 07:49:24 +0100

    implement the using of getResults

--- myside/tippspiel/views.py
+++ myside/tippspiel/views.py
@@ -42,19 +42,19 @@ def event(request, id):  # Event anzeigen
                     M = Match(team1=match["team1"], team2=match["team2"], finished=False,
                             id=match["id"], result1=-1, result2=-1, event=Event.objects.get(id=id), url=matchURL)
                     M.save()
-        for match in event:
-            if not match.finished:
-                result = sniffer.getResult(match.url)
-
-                if result[0]:
-                    match.finished = True
-                    match.result1 = bool(result[1])
-                    match.result2 = not bool(result[1])
+        results=sniffer.getResults(id):
+            for result in results:
+                match=Match.objects.get(id=int(result[0].split("/")[2]))
+                if not match.finished:
+                    match.finished=True
+                    match.result1 =bool(result[1])
+                    match.result2 =not bool(result[1])
                     match.save()
                     for user in User.objects.all():
                         if Tip.objects.get(user=user, match=match).tip == bool(match.result2):
                             Point.objects.get(
                                 user=user, event=match.event).points += 1
+                            Points.save()
 
         return render(request, "tippspiel/event.html", {"title": Event.objects.get(id=id).title, "matches": Match.objects.filter(event__id__contains=id, finished=False), "username": request.user.get_username, "points": Point.objects.get(user=request.user, event=Event.objects.get(id=id)).points, "result": Match.objects.filter(event__id__contains=id, finished=True)})
     else:
